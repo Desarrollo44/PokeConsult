22639dc135ea7c5ac62415c6c89690c84c56fca3
Desarrollo44 <david.avendano@ingeal.com>
2024-03-18 16:42:39 -0500

    avance de paginacion simple

--- src/components/pokemons.jsx
+++ src/components/pokemons.jsx
@@ -1,6 +1,8 @@
-import { Typography, Box, CircularProgress } from "@mui/material";
+import { Typography, Box, CircularProgress, Button } from "@mui/material";
 import { useState, useEffect } from "react";
 import SearchIcon from '@mui/icons-material/Search';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import axios from "axios";
 import Fade from '@mui/material/Fade';
 
@@ -9,11 +11,33 @@ function Pokemons({ param }) {
     const [pokeParam, setPokeParam] = useState('');
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState('');
+    const [offSetData, setOffSetData] = useState(0);
+    const [pokedata, setPokedata] = useState([]);
+
+    useEffect(() => {
+        pokeFech();
+    }, [offSetData]);
+
+    async function pokeFech () {
+        setLoading(true);
+        try {
+            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=20&offset=${offSetData}`);
+            setPokedata(response.data.results);
+            console.log(pokedata);
+            setLoading(false);
+            //console.log(pokedata[0].name);
+        } catch (error) {
+            setLoading(false);
+            console.log(`error: ${error}`);
+        }
+    };
 
     useEffect(() => {
         setPokeParam(param);
     }, [param]);
 
+
+    //https://pokeapi.co/api/v2/pokemon?limit=20 hacer paginacion de pokemon
     useEffect(() => {
         async function fetchData() {
             setLoading(true);
@@ -32,6 +56,8 @@ function Pokemons({ param }) {
             fetchData();
         }
     }, [pokeParam]);
+
+
     if (loading) {
         return (<Box
             display={'flex'}
@@ -46,87 +72,109 @@ function Pokemons({ param }) {
         </Box>);
     }
 
+    const handleOnClick = () => {
+       setOffSetData(offSetData + 20);
+       
+    }
+    const handleBack=()=>{
+        setOffSetData(offSetData-20);
+       
+    }
 
     return (
         <Box>
-            {pokeParam === '' ? (
 
-                <Box
-                    
-                    display={'flex'}
-                    style={{ margin: '0 auto' }}
-                    width={400}
-                    height={100}
-                    padding={5}
-                    alignItems='center'
+            <Box>
+                {pokeParam === '' ? (
 
-                >
-                    <SearchIcon style={{ fontSize: '120px' }} />
-                    <Typography variant="h4">por favor realice una consulta</Typography>
-                </Box>
-            ) : (
-                error === '' ? (
-                   <Fade in={error === ''} timeout={2000}>
                     <Box
+
                         display={'flex'}
-                        flexDirection={'column'}
-                        justifyContent={'center'}
-                        width={600}
-                        padding={3}
-                        margin={6}
-                        gap={3}
-                    >
-                        <Typography variant="h4">{`Pokemon's name: ${pokeConsult.name}`}</Typography>
-                        <Typography variant="h5">{`Pokedex's number: ${pokeConsult.id}`}</Typography>
-                        <Box display={'flex'} flexDirection={'row'} gap={6}>
-                            <Box>
-                                <Typography variant="h5">Types:</Typography>
-                                {pokeConsult.types && pokeConsult.types.map(types => (
-                                    <Typography marginLeft={2}><li>{types.type.name}</li></Typography>
-                                ))}
-                            </Box>
-                            <Box>
-                                <Typography variant="h5">Abilities:</Typography>
-                                {pokeConsult.types && pokeConsult.abilities.map(abilities => (
-                                    <Typography marginLeft={2}><li>{abilities.ability.name}</li></Typography>
-                                ))}
-                            </Box>
-                        </Box>
-                        <Box
-                            display={'flex'}
-                            flexDirection={'row'}
-                            gap={5}
-                            margin={5}
-                        >
-                            <Box>
-                                <Typography variant="h5">Sprite default:</Typography>
-                                <img width={'300px'} height={'300px'} alt={pokeConsult.name} src={pokeConsult.sprites?.front_default} />
-                            </Box>
-                            <Box>
-                                <Typography variant="h5">Sprite shiny:</Typography>
-                                <img width={'300px'} height={'300px'} alt={pokeConsult.name} src={pokeConsult.sprites?.front_shiny} />
-                            </Box>
-                        </Box>
-                    </Box>
-                </Fade>
-                ) : (
-                    <Box
-                        width={600}
-                        paddingTop={10}
-                        margin={'0 auto'}
-                        color={'red'}
-                    >
-                        <Fade in={error!==''} timeout={2000}>
-                            <div>
-                                <Typography>{error}</Typography>
-                                {/* Otro contenido aquí, como imágenes o videos */}
-                                <img style={{ marginLeft: '6rem' }} src="https://i.pinimg.com/originals/7b/d6/ab/7bd6abf0cb4502e87fd70fad35c66184.gif" alt="error" loop />
-                            </div>
-                        </Fade>
+                        style={{ margin: '0 auto' }}
+                        width={400}
+                        height={100}
+                        padding={5}
+                        alignItems='center'
 
+                    >
+                        <SearchIcon style={{ fontSize: '120px' }} />
+                        <Typography variant="h4">por favor realice una consulta</Typography>
                     </Box>
-                )
-            )}
+                ) : (
+                    error === '' ? (
+                        <Fade in={error === ''} timeout={2000}>
+                            <Box
+                                display={'flex'}
+                                flexDirection={'column'}
+                                justifyContent={'center'}
+                                width={600}
+                                padding={3}
+                                margin={6}
+                                gap={3}
+                            >
+
+
+                                <Typography variant="h4">{`Pokemon's name: ${pokeConsult.name}`}</Typography>
+                                <Typography variant="h5">{`Pokedex's number: ${pokeConsult.id}`}</Typography>
+                                <Box display={'flex'} flexDirection={'row'} gap={6}>
+                                    <Box>
+                                        <Typography variant="h5">Types:</Typography>
+                                        {pokeConsult.types && pokeConsult.types.map(types => (
+                                            <Typography marginLeft={2}><li>{types.type.name}</li></Typography>
+                                        ))}
+                                    </Box>
+                                    <Box>
+                                        <Typography variant="h5">Abilities:</Typography>
+                                        {pokeConsult.types && pokeConsult.abilities.map(abilities => (
+                                            <Typography marginLeft={2}><li>{abilities.ability.name}</li></Typography>
+                                        ))}
+                                    </Box>
+                                </Box>
+                                <Box
+                                    display={'flex'}
+                                    flexDirection={'row'}
+                                    gap={5}
+                                    margin={5}
+                                >
+                                    <Box>
+                                        <Typography variant="h5">Sprite default:</Typography>
+                                        <img width={'300px'} height={'300px'} alt={pokeConsult.name} src={pokeConsult.sprites?.front_default} />
+                                    </Box>
+                                    <Box>
+                                        <Typography variant="h5">Sprite shiny:</Typography>
+                                        <img width={'300px'} height={'300px'} alt={pokeConsult.name} src={pokeConsult.sprites?.front_shiny} />
+                                    </Box>
+                                </Box>
+                            </Box>
+                        </Fade>
+                    ) : (
+                        <Box
+                            width={600}
+                            paddingTop={10}
+                            margin={'0 auto'}
+                            color={'red'}
+                        >
+                            <Fade in={error !== ''} timeout={2000}>
+                                <div>
+                                    <Typography>{error}</Typography>
+                                    {/* Otro contenido aquí, como imágenes o videos */}
+                                    <img style={{ marginLeft: '6rem' }} src="https://i.pinimg.com/originals/7b/d6/ab/7bd6abf0cb4502e87fd70fad35c66184.gif" alt="error" loop />
+                                </div>
+                            </Fade>
+
+                        </Box>
+                    )
+
+                )}
+
+            </Box>
+            <Button variant="outlined" onClick={handleBack} ><ArrowBackIosIcon/></Button>
+            <Button variant="outlined" onClick={handleOnClick} ><ArrowForwardIosIcon/></Button>
+            {pokedata.map((data,index) => (
+                <Box key={index}> {/* Asegúrate de incluir un key único para cada elemento */}
+                    <Typography variant="body1">{`nombre ${data.name}`}</Typography>
+                </Box>
+            ))}
         </Box>
     );
 }
